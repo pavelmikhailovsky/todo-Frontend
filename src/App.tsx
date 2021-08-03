2a75223d8f16b2a3d1ea45fa58c7758adf84a6ba
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-08-03 21:50:22 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
+import { Route } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Registration from './components/Registration/Registration';
 
 // @ts-ignore
 function App(props) {
   return (
     <div className="wrapper">
       <Header />
-      <Content />
+      <Route path='/content' component={Content}/>
+      <Route path='/login' component={Login}/>
+      <Route path='/registration' component={Registration}/>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+console.log(store.getState())
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
